b90df1cbd894835bd2dab9cafd14b3163b4f23e4
Premal Patel <premalp@udel.edu>
2025-03-08 17:38:11 -0800

    add script to parse whatsapp messages

---
+++ main.py
@@ -0,0 +1,30 @@
+import re
+import json
+from collections import defaultdict
+
+SENDER_RE = r'\[\d{1,2}\/\d{1,2}\/\d{2},\s\d{1,2}:\d{2}:\d{2}.[AP]M\]\s(\w+\s\w+):\s(.+)'
+
+
+def parse_data() -> dict:
+    messages = defaultdict(list)
+    last_sender = ''
+    with open('data/_chat.txt', 'r') as file:
+        for line in file:
+            if match := re.match(SENDER_RE, line):
+                messages[match.group(1)].append(match.group(2))
+                last_sender = match.group(1)
+            elif len(messages) > 0:
+                messages[last_sender][-1] += line
+    return messages
+
+
+def write_message_data():
+    messages = parse_data()
+    for sender in messages:
+        print(f'{sender}: {len(messages[sender])}')
+    with open('data/chat.json', 'w') as f:
+        json.dump(messages, f, indent=4)
+
+
+if __name__ == '__main__':
+    write_message_data()
